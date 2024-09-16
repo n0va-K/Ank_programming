788b7dde98711c55046195228157027ff02cb18f
n0va-K <aternume@gmail.com>
2024-09-16 17:17:41 +0500

    Программа вычисления суммы двух целых чисел

---
+++ example/example/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое слагаемое");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе слагаемое");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Сумма чисел равна " + (x+y));
+
+            Console.ReadKey();
+
+        }
+    }
+}

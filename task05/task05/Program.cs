4b6832b5301454fc5a97bd34922804633ac096a2
Екатерина Анкудинова <katy231204@gmail.com>
2024-10-09 12:07:51 +0500

    5 задание выполнено (№2)

---
+++ task05/task05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = F(5, 7) * F(12, 8) * F(31, 2);
+            Console.WriteLine($"x = {x:F3}");
+            Console.ReadKey();
+        }
+
+        static double F(double a, double b)
+        {
+            return (a + Math.Sqrt(a)) / (b + Math.Sqrt(b));
+        }
+    }
+}

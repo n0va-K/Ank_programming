90a3450543125d221c96daa0f8f711a73c73fa1e
n0va-K <aternume@gmail.com>
2024-11-11 15:11:39 +0500

    Задание 8 выполнено

---
+++ task08/task08/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение аргумента функции: ");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"f({x})={MyFunction(x)}");
+
+            Console.ReadKey();
+        }
+
+        static double MyFunction(double x)
+        {
+            if (x <= 0)
+                return 0;
+            else if (x > 1)
+                return Math.Pow(x, 2);
+
+            return x;
+        }
+    }
+}
